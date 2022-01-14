b1ec64405f2246e69df227bc941d5fb07aaf53f3
Pablo Raphael <pabloraphaelmoreira@outlook.com>
2022-01-14 17:42:35 -0300

    NavBar

---
+++ frontweb/src/components/NavBar/index.tsx
@@ -0,0 +1,23 @@
+import { ReactComponent as GithubIcon } from 'assets/img/Github.svg'
+import './style.css'
+
+function NavBar() {
+
+    return (
+        <header>
+            <nav className='container'>
+                <div className='dsmovie-nav-content'>
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/pablomoreiraferrari">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/pablomoreiraferrari</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default NavBar;
